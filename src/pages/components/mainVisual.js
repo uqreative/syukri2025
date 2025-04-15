379ded8cd128c8b994b72b7f9ed6e7fc3186a042
uqreative <syukri.gazali.mohammad@gmail.com>
2025-04-15 18:58:42 +0700

    Add component
    - add header component
    - add footer component
    - add mainVisual component and css modul
    - add install swiperJS

--- src/pages/components/mainVisual.js
+++ src/pages/components/mainVisual.js
@@ -1,7 +1,31 @@
+import styles from './mainVisual.module.css'
+import Swiper from 'swiper';
+// import '@swiper/css';
+
+const swiper = new Swiper('.swiper', { 
+    slidesPerView: 1, 
+    spaceBetween: 30, 
+  });
+
 export default function MainVisual(){
     return(
-        <section className="main-visual">
-            <h1>Main Visual</h1>
+        <section className={styles.mainvisual}>
+            {/* <h1>Main Visual</h1> */} 
+            <div className="swiper"> 
+                <div className="swiper-wrapper"> 
+                    <div className="swiper-slide"> 
+                        <h1 className={styles.testkelas}>Lorem Ipsum Et Dolor Sit Amet</h1>
+                    </div>
+                    <div className="swiper-slide">Slide 2</div>
+                    <div className="swiper-slide">Slide 3</div> 
+                </div> 
+                <div className="swiper-pagination"></div>
+ 
+                <div className="swiper-button-prev"></div>
+                <div className="swiper-button-next"></div>
+ 
+                <div className="swiper-scrollbar"></div>
+            </div>
         </section>
     );
 }
